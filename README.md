0784dc853456e1a355a1df5739d8493bf32e0ba5
David Figueroa <dgfigueroa29@gmail.com>
2019-10-15 18:57:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SaltoInicial
+ InAppWeb
---
+++ app/src/main/java/com/boa/utils/Common.java
@@ -0,0 +1,13 @@
+package com.boa.utils;
+
+import android.Manifest;
+
+/**
+ * Created by Boa (davo.figueroa14@gmail.com) on 18 oct 2017.
+ */
+public class Common{
+	//En true desbloquea la impresión de trackers y variables para debug
+	public static final boolean		DEBUG		= false;
+	public static final String[]	PERMISSIONS	= {	Manifest.permission.INTERNET};
+	public static final String		WEB			= "http://www.saltoinicial.com.ar/";
+}
