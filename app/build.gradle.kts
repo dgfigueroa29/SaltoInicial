25a866c312fe52d707f7006d509f954a3e6e37d7
David Figueroa <dgfigueroa29@dgfigueroa29-Z790-EAGLE-AX>
2026-01-15 19:40:21 -0300

    Update kotlinc.xml, misc.xml, build.gradle.kts, and 2 more files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,13 +34,13 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_19
-        targetCompatibility = JavaVersion.VERSION_19
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     @Suppress("DEPRECATION")
     kotlinOptions {
         @Suppress("DEPRECATION")
-        jvmTarget = "19"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
